be74e36cf9fa82a4ba405e554812fa923c59d693
jomerz <johannes.merz@rwth-aachen.de>
2018-04-09 13:48:21 +0200

    Added skeleton classes

--- ZTTPOL2016/scripts/zttpol2016.py
+++ ZTTPOL2016/scripts/zttpol2016.py
@@ -18,6 +18,7 @@ import CombineHarvester.ZTTPOL2016.zttpol2016_systematics as zttpol_systematics
 #Colors
 OKGREEN = '\033[92m'
 WARNING = '\033[93m'
+FAIL = '\033[91m'
 ENDC = '\033[0m'
 
 def CreateDatacard(zttdatacards):
@@ -25,13 +26,16 @@ def CreateDatacard(zttdatacards):
     try:
         datacards = zttdatacards.ZttPolarisationDatacards()
 
-        
+        #Modify the datacard
+        datacards.cb
+
     except:
-        print "CreateDatacard failed:"
+        print FAIL + "Extracting Shapes from input files failed:" + ENDC
         (ty, val, tb)=sys.exc_info()
-        print WARNING + "Err Type : " + ENDC, ty
-        print WARNING + "Err Value: " + ENDC, val
-        print WARNING + "Trace    : " + ENDC, tb
+        print FAIL + "Error on line :" + ENDC + '{}'.format(sys.exc_info()[-1].tb_lineno)
+        print FAIL + "Err Type      :" + ENDC, ty
+        print FAIL + "Err Value     :" + ENDC, val
+        print FAIL + "Trace         :" + ENDC, tb
 
     return datacards
 
@@ -56,25 +60,76 @@ def WriteDatacard():
 
     return None
 
+def hp_inputsfromsamples():
+
+    return None
 
 
-
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ MAIN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~:
 if __name__ == "__main__":
     #Arguments for the ArgParse parser
     parser = argparse.ArgumentParser(description="Create ROOT inputs and datacards for ZTT polarisation analysis.",
                                      parents=[logger.loggingParser])
 
     parser.add_argument("-i", "--input-dir", required=False,
-                        help="Input directory.")
+                        help="Input directory of the root files. In case of HP for the samples.")
+    parser.add_argument("-c", "--channel", action = "append",
+                        default=["mt", "et", "tt", "em"],
+                       help="Channel. This agument can be set multiple times. [Default: %(default)s]")
+    parser.add_argument("--categories", action="append", nargs="+",
+                        default=[["all"]] * len(parser.get_default("channel")),
+                       help="Categories per channel. This agument needs to be set as often as --channels. [Default: %(default)s]")
+    parser.add_argument("--HarryPlotter", default = False,
+                        help = "Use HarryPlotter for extracting shapes")
 
-    #Create Datacards
+    args = parser.parse_args()
+
+    #1.Create Datacards
     print OKGREEN + '----- Creating datacard with processes and systematics...    -----' + ENDC
+
     datacards = CreateDatacard(zttdatacards)
 
-    print OKGREEN + '----- Adding systematic uncertainties...                     -----' + ENDC
+    datacards.cb.channel(args.channel)
 
+    for index, (channel, categories) in enumerate(zip(args.channel, args.categories)):
+
+        # prepare category settings based on args and datacards
+        if (len(categories) == 1) and (categories[0] == "all"):
+            categories = datacards.cb.cp().channel([channel]).bin_set()
+        else:
+            categories = list(set(categories).intersection(set(datacards.cb.cp().channel([channel]).bin_set())))
+        args.categories[index] = categories
+
+        # restrict CombineHarvester to configured categories:
+        datacards.cb.FilterAll(lambda obj : (obj.channel() == channel) and (obj.bin() not in categories))
+
+    #DEBUG
+    print WARNING + "datacards: channel_set" + ENDC, datacards.cb.channel_set()
+    print WARNING + "datacards: bin_set" + ENDC, datacards.cb.bin_set()
+
+    #2.Extract shapes from input root files or from samples with HP
     print OKGREEN + '----- Extracting histograms from input root files...         -----' + ENDC
+
+    if not args.HarryPlotter:
+        try:
+            for chn in datacards.cb.channel_set():
+                file = args.input_dir + "ztt_" + chn + ".root"
+                datacards.cb.cp().channel([chn]).backgrounds().ExtractShapes(
+                    file, '$BIN/$PROCESS', '$BIN/$PROCESS_$SYSTEMATIC'
+                )
+                datacards.cb.cp().channel([chn]).backgrounds().ExtractShapes(
+                    file, '$BIN/$PROCESS', '$BIN/$PROCESS_$SYSTEMATIC'
+                )
+        except Exception as e:
+            print FAIL + "Extracting Shapes from input files failed:" + ENDC
+            (ty, val, tb)=sys.exc_info()
+            print FAIL + "Error on line :" + ENDC + '{}'.format(sys.exc_info()[-1].tb_lineno)
+            print FAIL + "Err Type      :" + ENDC, ty
+            print FAIL + "Err Value     :" + ENDC, val
+            print FAIL + "Trace         :" + ENDC, tb
+    else:
+        hp_inputsfromsamples()
+
 
     print OKGREEN + '----- Scaling signal process rates...                        -----' + ENDC
 
