ab42ddbe1f58ed93a6864aca50f843e521a9b83f
jomerz <johannes.merz@rwth-aachen.de>
2018-04-18 16:17:53 +0200

    Structural changes

---
+++ ZTTPOL2016/python/tools.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+import array
+import copy
+import fcntl
+import multiprocessing
+import os
+import re
+import sys
+import termios
+import textwrap
+import shlex
+import subprocess
+import time
+import ROOT
+import math
+
+
+
+class ProgressIterator(object):
+
+	def __init__(self, iterable, length=None, description="", visible=True):
+		self.iterator = iter(iterable)
+		self.len = length if length != None else len(iterable)
+		self.description = (description if description == "" else (description+"... "))
+		self.current_index = 0
+		self.ratio = -1.0
+		self.tty_width = get_tty_size()[1]
+		self.visible = visible
+
+	def __iter__(self):
+		return self
+
+	def next(self):
+		ratio = float(self.current_index) / self.len
+
+		if int(ratio*100) > int(self.ratio*100):
+			self.ratio = ratio
+			current_progress = min(int(math.ceil(float(self.tty_width) * self.current_index / self.len)),
+		                       self.tty_width)
+
+			line = "%s%.1f %s" % (self.description, ratio*100, "%")
+			#line += (" " * (self.tty_width - len(line)))
+			line = line.center(self.tty_width)
+			line = "\r\033[0;42m%s\033[0;41m%s\033[0m" % (line[:current_progress], line[current_progress:])
+			if self.visible:
+				sys.stdout.write(line)
+				sys.stdout.flush()
+
+		if (self.current_index == self.len-1) and self.visible:
+			sys.stdout.write("\r\033[J")
+			sys.stdout.flush()
+
+		self.current_index += 1
+		try:
+			return self.iterator.next()
+		except StopIteration, e:
+			sys.stdout.write("\r\033[J")
+			sys.stdout.flush()
+			raise StopIteration(e)
+
+
+
+
+def get_tty_size():
+	size = array.array("B", [0, 0, 0, 0])
+	try:
+		fcntl.ioctl(0, termios.TIOCGWINSZ, size, True)
+		return (size[0], size[2])
+	except:
+		return size
+
+def parallelize(function, arguments_list, n_processes=1, description=None):
+	if n_processes <= 1:
+		results = []
+		for arguments in arguments_list:
+			results.append(function(arguments))
+		return results
+	else:
+		pool = multiprocessing.Pool(processes=max(1, min(n_processes, len(arguments_list))))
+		results = pool.map_async(function, arguments_list, chunksize=1)
+		n_tasks = len(arguments_list)
+		left = n_tasks-1
+		progress_iterator = ProgressIterator(range(n_tasks), description=(description if description else "calling "+str(function)))
+		progress_iterator.next()
+		while (True):
+			ready = results.ready()
+			remaining = results._number_left
+			if ready or (remaining < left):
+				for i in range(left-remaining):
+					progress_iterator.next()
+				left = remaining
+			if ready:
+				break
+			time.sleep(1.0)
+		returnvalue = results.get(9999999)
+		pool.close() # necessary to actually terminate the processes
+		pool.join()  # without these two lines, they happen to live until the whole program terminates
+		return returnvalue
+
+def _call_command(args):
+	command = None
+	cwd = None
+	if isinstance(args, basestring):
+		command = args
+	else:
+		command = args[0]
+		if len(args) > 1:
+			cwd = args[1]
+
+	old_cwd = None
+	if not cwd is None:
+		old_cwd = os.getcwd()
+		os.chdir(cwd)
+	print command
+	#log.debug(command)
+	#logger.subprocessCall(command, shell=True)
+
+	if not cwd is None:
+		os.chdir(old_cwd)
--- ZTTPOL2016/scripts/zttpol2016.py
+++ ZTTPOL2016/scripts/zttpol2016.py
@@ -18,6 +18,8 @@ import CombineHarvester.ZTTPOL2016.zttpol2016_datacards as zttdatacards
 import CombineHarvester.ZTTPOL2016.tools as tools
 from CombineHarvester.ZTTPOL2016.tools import _call_command
 
+from CombineHarvester.ZTTPOL2016.zttpol2016_functions import *
+
 #Colors
 HEADER = '\033[95m'
 OKBLUE = '\033[94m'
@@ -27,68 +29,6 @@ FAIL = '\033[91m'
 ENDC = '\033[0m'
 BOLD = '\033[1m'
 UNDERLINE = '\033[4m'
-
-def CreateDatacard():
-    '''Create an instance of the python datacards modul, which includes a combine harvester instance.'''
-
-    datacards = zttdatacards.ZttPolarisationDatacards()
-
-    return datacards
-
-
-def ExtractShapes(datacards,input_dir):
-    '''Extract the Shape uncertainties from root input histograms. '''
-    #Find the files present in the input_dir
-    files_in_input_dir = [file.split("_") for file in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, file))]
-    categories_in_input_dir = [reduce( lambda x,y: x + "_" + y, strings[2:-1]) for strings in files_in_input_dir]
-
-    for chn in datacards.cb.channel_set():
-        bins = filter(lambda bin: bin.split('_')[0] == chn,datacards.cb.bin_set())
-        bins = filter(lambda x: categories_in_input_dir.count(x) >= 1, bins)
-        for bin in bins:
-            try:
-                #Background shapes
-                file = args.input_dir + "/ztt_" + chn +"_" + bin + "_13TeV.root"
-                datacards.cb.cp().channel([chn]).bin([bin]).backgrounds().ExtractShapes(
-                   file, '$BIN/$PROCESS', '$BIN/$PROCESS_$SYSTEMATIC'
-                )
-                #Signal shapes
-                datacards.cb.cp().channel([chn]).bin([bin]).signals().ExtractShapes(
-                    file, '$BIN/$PROCESS', '$BIN/$PROCESS'
-                )
-                print OKGREEN + 'Extracting Shapes for:' + ENDC, bin
-            except Exception as e:
-                print FAIL + "Extracting Shapes from input files failed:" + ENDC
-                (ty, val, tb)=sys.exc_info()
-                print FAIL + "Error on line :" + ENDC + '{}'.format(sys.exc_info()[-1].tb_lineno)
-                print FAIL + "Err Type      :" + ENDC, ty
-                print FAIL + "Err Value     :" + ENDC, val
-                print FAIL + "Trace         :" + ENDC, tb
-
-    return datacards
-
-
-def BinErrorsAndBBB(datacards, AddThreshold, MergeTreshold, FixNorm):
-    '''Function to add bin by bin uncertainties. '''
-
-    bbb = ch.BinByBinFactory()
-    bbb.SetAddThreshold(AddThreshold).SetMergeThreshold(MergeTreshold).SetFixNorm(FixNorm)
-    bbb.MergeBinErrors(datacards.cb.cp().backgrounds())
-    bbb.AddBinByBin(datacards.cb.cp().backgrounds(), datacards.cb)
-
-    datacards.cb.SetGroup("bbb", [".*_bin_\\d+"])
-
-    return None
-
-
-def WriteDatacard(datacards,output_dir):
-    ''' Write datacards. '''
-
-    writer = ch.CardWriter(os.path.join(output_dir + "/zttpol_datacard.txt"),
-                           os.path.join(output_dir + "/zttpol_datacard_rootfile.root"))
-
-    return writer.WriteCards(output_dir, datacards.cb)
-
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ MAIN ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~:
 if __name__ == "__main__":
@@ -194,8 +134,7 @@ if __name__ == "__main__":
             os.system(command)
 
         datacards_workspaces = {datacard : os.path.splitext(datacard)[0]+"_workspace"+".root" for datacard in datacards_cbs.keys()}
-
-
+        print OKGREEN + "Done " + ENDC
 
         #7.-----totstatuncs
         print WARNING + '-----      Tot and stat uncs                                          -----' + ENDC
