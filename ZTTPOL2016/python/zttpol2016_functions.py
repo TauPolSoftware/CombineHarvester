0389962d98c107fa36a6e4ec67971b0018854334
jomerz <johannes.merz@rwth-aachen.de>
2018-05-08 17:12:58 +0200

    Moved the MultiDimFit to functions

--- ZTTPOL2016/python/zttpol2016_functions.py
+++ ZTTPOL2016/python/zttpol2016_functions.py
@@ -142,14 +142,69 @@ def use_asimov_dataset(datacards, pol=-0.159, r=1.0, signal_mass = None, signal_
     negpol_signals = datacards.cb.cp().signals()
     negpol_signals.FilterAll(lambda obj : ("negpol" not in obj.process().lower()))
 
-
     pospol_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (r * (1.0 + pol) / 2.0)))
     negpol_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() * (r * (1.0 - pol) / 2.0)))
-
+    
     datacards.cb.cp().ForEachObs(_replace_observation_by_asimov_dataset)
+    
+    pospol_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() / (r * (1.0 + pol) / 2.0)))
+    negpol_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() / (r * (1.0 - pol) / 2.0)))
 
-    pospol_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() / (r * (1.0 + pol))))
-    negpol_signals.ForEachProc(lambda process: process.set_rate(process.no_norm_rate() / (r * (1.0 - pol))))
+
+    return datacards
 
 
+def MultiDimFit_TotStatUnc(datacards, datacards_workspaces, datacards_cbs, args,algo = "singles"):
+    
+    stable_options = r"--robustFit 1 --preFitValue 1.0 --cminDefaultMinimizerType Minuit2 --cminDefaultMinimizerAlgo Minuit2 --cminDefaultMinimizerStrategy 0 --cminFallbackAlgo Minuit2,0:1.0"
+
+    method = "MultiDimFit"
+    tmp_args = "-M MultiDimFit --algo " + algo + " -P pol --redefineSignalPOIs pol "+stable_options+" -n "
+    name = re.search("(-n|--name)[\s=\"\']*(?P<name>\w*)[\"\']?\s", tmp_args)
+    name = name.groupdict()["name"]
+    datacards_poi_ranges = {}
+
+    chunks = [[chunk*199, (chunk+1)*199-1] for chunk in xrange(200/199+1)]
+
+    split_stat_syst_uncs_options = [
+        "--saveWorkspace",
+        "--snapshotName {method} -w w".format(method=method),
+        "--snapshotName {method} -w w --freezeNuisanceGroups syst".format(method=method, uncs="{uncs}"), #.format(uncs=datacards_cbs[datacard].syst_name_set())
+    ]
+    split_stat_syst_uncs_names = [
+        "Workspace",
+        "TotUnc",
+        "StatUnc",
+    ]
+
+    for split_stat_syst_uncs_index, (split_stat_syst_uncs_option, split_stat_syst_uncs_name) in enumerate(zip(split_stat_syst_uncs_options, split_stat_syst_uncs_names)):
+        prepared_tmp_args = None
+
+        new_name = ("" if name is None else name) + " " + split_stat_syst_uncs_name
+        if name is None:
+            prepared_tmp_args = tmp_args +new_name
+        else:
+            prepared_tmp_args = copy.deepcopy(tmp_args)
+            prepared_tmp_args = re.sub("(--algo)([\s=\"\']*)(\w*)([\"\']?\s)", "\\1\\2 "+("none" if split_stat_syst_uncs_index == 0 else "\\3")+"\\4", prepared_tmp_args)
+            prepared_tmp_args = re.sub("(-n|--name)([\s=\"\']*)(\w*)([\"\']?\s)", "\\1\\2"+new_name+"\\4", prepared_tmp_args)
+
+        commands = []
+        for chunk_index, (chunk_min, chunk_max) in enumerate(chunks):
+            commands.extend([[
+                    "combine -m {MASS} {POI_RANGE} {ARGS} {CHUNK_POINTS} {SPLIT_STAT_SYST_UNCS} {WORKSPACE}".format(
+                            MASS=[mass for mass in datacards_cbs[datacard].mass_set() if mass != "*"][0] if len(datacards_cbs[datacard].mass_set()) > 1 else 91,
+                            POI_RANGE="--rMin {RMIN} --rMax {RMAX}" if datacard in datacards_poi_ranges else "",
+                            ARGS=prepared_tmp_args.format(CHUNK=str(chunk_index), RMIN="{RMIN}", RMAX="{RMAX}"),
+                            CHUNK_POINTS = "" if (chunk_min is None) or (chunk_max is None) else "--firstPoint {CHUNK_MIN} --lastPoint {CHUNK_MAX}".format(
+                                    CHUNK_MIN=chunk_min,
+                                    CHUNK_MAX=chunk_max
+                            ),
+                            SPLIT_STAT_SYST_UNCS=split_stat_syst_uncs_option,
+                            WORKSPACE="-d " + workspace
+                    ).format(RMIN=datacards_poi_ranges.get(datacard, ["", ""])[0], RMAX=datacards_poi_ranges.get(datacard, ["", ""])[1]),
+                    os.path.dirname(workspace)
+            ] for datacard, workspace in datacards_workspaces.iteritems()])
+
+        tools.parallelize(_call_command, commands, n_processes=4, description="combine")
+    
     return datacards
