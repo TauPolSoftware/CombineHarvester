c07a90348f517626f1840cf636136d998b24f0e4
jomerz <johannes.merz@rwth-aachen.de>
2018-05-21 23:58:40 +0200

    Revert "Systematics"
    This reverts commit d01cacb1883f1d6a9a33957edb9ff2036d87ef52.

--- ZTTPOL2016/scripts/zttpol2016.py
+++ ZTTPOL2016/scripts/zttpol2016.py
@@ -104,7 +104,6 @@ if __name__ == "__main__":
     #5.-----Done
     print WARNING + '-----      '+OKGREEN+'Done'+WARNING +'                                                       -----' + ENDC
 
-
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ StatisticalAnalysis ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~:
     if args.analysis:
         print WARNING + UNDERLINE  + '-----      Performing statistical analysis                            -----' + ENDC
