e423615c13461879f08108ab432166079bac4684
Thomas Mueller <t.muller@cern.ch>
2018-06-26 18:08:36 +0200

    zttpol_nuisance_impacts.sh: get it running

--- ZTTPOL2016/scripts/zttpol_nuisance_impacts.sh
+++ ZTTPOL2016/scripts/zttpol_nuisance_impacts.sh
@@ -8,19 +8,25 @@ for WORKSPACE in `ls $1/best_choice/datacards/combined/workspace.root $1/workspa
 do
 	pushd `dirname ${WORKSPACE}`
 	
-		# initial fit
-		combineTool.py -M Impacts --doInitialFit --robustFit 1 -t -1 \
-			--setParameters pol=-0.159,r=1 --setParameterRanges pol=-0.2,-0.1:r=0.5,1.5 \
-			-m 0 -d `basename ${WORKSPACE}` --parallel 8
+	# initial fit
+	combineTool.py -M Impacts --doInitialFit --allPars \
+		--redefineSignalPOIs pol --setParameters pol=-0.159,r=1 --setParameterRanges pol=-0.2,-0.1:r=0.5,1.5 \
+		--robustFit 1 -t -1 \
+		-m 0 -d `basename ${WORKSPACE}` --parallel 8
 	
-		# fits for all nuisance parameters
-		combineTool.py -M Impacts --robustFit 1 -t -1 --doFits --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP \ #--minimizerAlgoForMinos Minuit2,Migrad \
-			--setParameters pol=-0.159,r=1 --setParameterRanges pol=-0.2,-0.1:r=0.5,1.5 \
-			-m 0 -d `basename ${WORKSPACE}` --parallel 8
-		
-		# collect and plot results
-		combineTool.py -M Impacts -m 0 -d `basename ${WORKSPACE}` -o impacts.json
-		plotImpacts.py -i impacts.json -o impacts
+	# fits for all nuisance parameters
+	combineTool.py -M Impacts --doFits --allPars \
+		--redefineSignalPOIs pol --setParameters pol=-0.159,r=1 --setParameterRanges pol=-0.2,-0.1:r=0.5,1.5 \
+		--robustFit 1 -t -1 --X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --setRobustFitAlgo Minuit2,Migrad \
+		-m 0 -d `basename ${WORKSPACE}` --parallel 8
+	
+	# collect results
+	combineTool.py -M Impacts --allPars \
+		--redefineSignalPOIs pol --setParameters pol=-0.159,r=1 --setParameterRanges pol=-0.2,-0.1:r=0.5,1.5 \
+		-m 0 -d `basename ${WORKSPACE}` -o impacts.json --parallel 8
+	
+	# plot results
+	plotImpacts.py -i impacts.json -o impacts
 	
 	popd
 done
