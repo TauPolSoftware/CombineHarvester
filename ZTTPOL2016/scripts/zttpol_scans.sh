bd6c7b7011d53567ade9b38d3a6cbf0980d1427d
Thomas Mueller <t.muller@cern.ch>
2018-07-10 18:02:52 +0200

    zttpol_scans.sh: added 2D scans

--- ZTTPOL2016/scripts/zttpol_scans.sh
+++ ZTTPOL2016/scripts/zttpol_scans.sh
@@ -35,7 +35,7 @@ then
 			--x-label "\"Average Polarisation #LTP_{#tau}#GT\"" --y-label "\"Likelihood -2#Deltaln L\"" --title "\"r floating\"" \
 			-m LP L L L LP L L L --marker-sizes 0.5 --line-styles 1 1 2 3 1 1 2 3 -C 1 1 1 1 2 2 2 2 \
 			--labels \"Stat. + Syst.\" \"\" \"\" \"\" \"Stat.\" \"\" \"\" \"\" --legend-markers LP --legend 0.32 0.72 0.7 0.9 \
-			--www $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol.tot_unc.scan.MultiDimFit.mH0.root@@g"` \
+			--www $2/`dirname ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g"` \
 			--filename higgsCombine.pol.scan.MultiDimFit.mH0 --formats pdf png \
 			--no-cache
 	
@@ -45,7 +45,7 @@ then
 			--x-label "\"Average Polarisation #LTP_{#tau}#GT\"" --y-label "\"Likelihood -2#Deltaln L\"" --title "\"r floating\"" \
 			-m LP L L L LP L L L --marker-sizes 0.5 --line-styles 1 1 2 3 1 1 2 3 -C 1 1 1 1 2 2 2 2 \
 			--labels "\"Stat. + Syst.\"" \"\" \"\" \"\" "\"Stat.\"" \"\" \"\" \"\" --legend-markers LP --legend 0.32 0.72 0.7 0.9 \
-			--www $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol.tot_unc.scan.MultiDimFit.mH0.root@@g"` \
+			--www $2/`dirname ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g"` \
 			--filename higgsCombine.pol.scan.MultiDimFit.mH0_zoom --formats pdf png \
 			--no-cache
 
@@ -83,7 +83,7 @@ for COMBINE_OUTPUT in $1/*/datacards/{individual/*/*,category/*,channel/*,combin
 			--x-label "\"Average Polarisation #LTP_{#tau}#GT\"" --y-label "\"Likelihood -2#Deltaln L\"" --title "\"r=1 fixed\"" \
 			-m LP L L L LP L L L --marker-sizes 0.5 --line-styles 1 1 2 3 1 1 2 3 -C 1 1 1 1 2 2 2 2 \
 			--labels \"Stat. + Syst.\" \"\" \"\" \"\" \"Stat.\" \"\" \"\" \"\" --legend-markers LP --legend 0.32 0.72 0.7 0.9 \
-			--www $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol_r1.tot_unc.scan.MultiDimFit.mH0.root@@g"` \
+			--www $2/`dirname ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g"` \
 			--filename higgsCombine.pol_r1.scan.MultiDimFit.mH0 --formats pdf png \
 			--no-cache
 	
@@ -93,8 +93,56 @@ for COMBINE_OUTPUT in $1/*/datacards/{individual/*/*,category/*,channel/*,combin
 			--x-label "\"Average Polarisation #LTP_{#tau}#GT\"" --y-label "\"Likelihood -2#Deltaln L\"" --title "\"r=1 fixed\"" \
 			-m LP L L L LP L L L --marker-sizes 0.5 --line-styles 1 1 2 3 1 1 2 3 -C 1 1 1 1 2 2 2 2 \
 			--labels "\"Stat. + Syst.\"" \"\" \"\" \"\" "\"Stat.\"" \"\" \"\" \"\" --legend-markers LP --legend 0.32 0.72 0.7 0.9 \
-			--www $2/`echo ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g" -e "s@higgsCombine.pol_r1.tot_unc.scan.MultiDimFit.mH0.root@@g"` \
+			--www $2/`dirname ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g"` \
 			--filename higgsCombine.pol_r1.scan.MultiDimFit.mH0_zoom --formats pdf png \
+			--no-cache
+
+	done | runParallel.py -n 8
+fi
+
+
+# ===== 2D scans ==================================================================================
+
+# combine
+
+# total uncertainty + saving best fit values for each parameter in a workspace
+combineTool.py -M MultiDimFit --algo grid --points 400 --setParameterRanges pol=-0.3,0:r=0.5,1.5 \
+	--saveWorkspace -n .pol_r.tot_unc.scan \
+	-d $1/*/datacards/{individual/*/*,category/*,channel/*,combined}/workspace.root \
+	--there -m 0 --parallel 8 \
+	--robustFit 1
+
+# statistical uncertainty
+combineTool.py -M MultiDimFit --algo grid --points 400 --setParameterRanges pol=-0.3,0:r=0.5,1.5 \
+	--skipInitialFit --fastScan -w w --snapshotName MultiDimFit --freezeNuisanceGroups syst_plus_bbb -n .pol_r.stat_unc.scan \
+	-d $1/*/datacards/{individual/*/*,category/*,channel/*,combined}/higgsCombine.pol_r.tot_unc.scan.MultiDimFit.mH0.root \
+	--there -m 0 --parallel 8 \
+	--robustFit 1
+
+# plotting
+
+if [ -x "$(command -v higgsplot.py)" ]
+then
+	for COMBINE_OUTPUT in $1/*/datacards/{individual/*/*,category/*,channel/*,combined}/higgsCombine.pol_r.tot_unc.scan.MultiDimFit.mH0.root; do
+	
+		echo higgsplot.py -i ${COMBINE_OUTPUT} -f limit \
+			-x pol -y r -z \"2*deltaNLL\" --tree-draw-options prof --x-bins 20,-0.3,0.0 --y-bins 20,0.5,1.5  \
+			--x-label "\"Average Polarisation #LTP_{#tau}#GT\"" --y-label "\"Signal Strength r\"" --z-label "\"Likelihood -2#Deltaln L\"" \
+			--title "\"Stat. + syst. uncertainties\"" -m COLZ --x-lims -0.3 0 --y-lims 0.5 1.5 --z-lims 0 10 \
+			--www $2/`dirname ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g"` \
+			--filename `basename ${COMBINE_OUTPUT} | sed -e "s@.root@_zoom@g"` --formats pdf png \
+			--no-cache
+
+	done | runParallel.py -n 8
+	
+	for COMBINE_OUTPUT in $1/*/datacards/{individual/*/*,category/*,channel/*,combined}/higgsCombine.pol_r.stat_unc.scan.MultiDimFit.mH0.root; do
+	
+		echo higgsplot.py -i ${COMBINE_OUTPUT} -f limit \
+			-x pol -y r -z \"2*deltaNLL\" --tree-draw-options prof --x-bins 20,-0.3,0.0 --y-bins 20,0.5,1.5  \
+			--x-label "\"Average Polarisation #LTP_{#tau}#GT\"" --y-label "\"Signal Strength r\"" --z-label "\"Likelihood -2#Deltaln L\"" \
+			--title "\"Stat. uncertainties\"" -m COLZ --x-lims -0.3 0 --y-lims 0.5 1.5 --z-lims 0 10 \
+			--www $2/`dirname ${COMBINE_OUTPUT} | sed -e "s@${1}/@@g"` \
+			--filename `basename ${COMBINE_OUTPUT} | sed -e "s@.root@_zoom@g"` --formats pdf png \
 			--no-cache
 
 	done | runParallel.py -n 8
